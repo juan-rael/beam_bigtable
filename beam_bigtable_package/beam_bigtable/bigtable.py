b8b7a669c7531e1c33e7cf2c7fceb0094116fcb8
Juan Rael <juan@qlogic.io>
2019-02-06 15:01:54 -0500

    Test Million Rows Both

--- beam_bigtable_package/beam_bigtable/bigtable.py
+++ beam_bigtable_package/beam_bigtable/bigtable.py
@@ -36,6 +36,7 @@ those generated rows in the table.
                                   table_id))
 """
 from __future__ import absolute_import
+from __future__ import division
 
 import copy
 import math
@@ -46,213 +47,12 @@ from apache_beam.io.range_trackers import LexicographicKeyRangeTracker
 from apache_beam.metrics import Metrics
 from apache_beam.transforms.display import DisplayDataItem
 
-
 try:
-  from google.cloud._helpers import _microseconds_from_datetime
-  from google.cloud._helpers import UTC
-  from google.cloud.bigtable import row
   from google.cloud.bigtable import Client
-  from google.cloud.bigtable import column_family
-  from google.cloud.bigtable import enums 
 except ImportError:
   pass
 
-__all__ = ['WriteToBigTable','ReadFromBigTable']
-
-
-class _BigTableReadFn(iobase.BoundedSource):
-  """ Creates the connector can call and add_row to the batcher using each
-  row in beam pipe line
-  Args:
-    project_id(str): GCP Project ID
-    instance_id(str): GCP Instance ID
-    table_id(str): GCP Table ID
-  """
-  def __init__(self, project_id, instance_id, table_id,
-               row_set=None, filter_=None):
-    """ Constructor of the Read Source of Bigtable
-    Args:
-      project_id(str): GCP Project of to write the Rows
-      instance_id(str): GCP Instance to write the Rows
-      table_id(str): GCP Table to write the `DirectRows`
-      row_set():
-      filter_:
-    """
-    super(self.__class__, self).__init__()
-    from apache_beam.metrics import Metrics
-    self.beam_options = {'project_id': project_id,
-                         'instance_id': instance_id,
-                         'table_id': table_id,
-                         'row_set': row_set,
-                         'filter_': filter_}
-    self.table = None
-    self.read_row = Metrics.counter(self.__class__, 'read_row')
-
-  def _getTable(self):
-    if self.table is None:
-      options = self.beam_options
-      client = Client(project=self.beam_options['project_id'])
-      instance = client.instance(self.beam_options['instance_id'])
-      self.table = instance.table(self.beam_options['table_id'])
-    return self.table
-
-  def get_sample_row_keys(self):
-    return self._getTable().sample_row_keys()
-
-  def get_range_tracker(self, start_position, stop_position):
-    return LexicographicKeyRangeTracker(start_position, stop_position)
-
-  def split(self,
-            desired_bundle_size,
-            start_position=None,
-            stop_position=None):
-
-    if self.beam_options['row_set'] is not None:
-      for sample_row_key in self.beam_options['row_set'].row_ranges:
-        sample_row_keys = self.get_sample_row_keys()
-        for row_split in self.split_range_size(desired_bundle_size,
-                                               sample_row_keys,
-                                               sample_row_key):
-          yield row_split
-    else:
-      suma = 0
-      last_offset = 0
-      current_size = 0
-
-      start_key = b''
-      end_key = b''
-
-      sample_row_keys = self.get_sample_row_keys()
-      for sample_row_key in sample_row_keys:
-        current_size = sample_row_key.offset_bytes-last_offset
-        if suma >= desired_bundle_size:
-          end_key = sample_row_key.row_key
-          for fraction in self.range_split_fraction(suma,
-                                                    desired_bundle_size,
-                                                    start_key, end_key):
-            yield fraction
-          start_key = sample_row_key.row_key
-
-          suma = 0
-        suma += current_size
-        last_offset = sample_row_key.offset_bytes
-
-  def split_range_size(self, desired_size, sample_row_keys, range_):
-    start, end = None, None
-    l = 0
-    for sample_row in sample_row_keys:
-      current = sample_row.offset_bytes - l
-      if sample_row.row_key == b'':
-        continue
-
-      if(range_.start_key <= sample_row.row_key and
-         range_.end_key >= sample_row.row_key):
-        if start is not None:
-          end = sample_row.row_key
-          range_tracker = LexicographicKeyRangeTracker(start, end)
-
-          for split_key_range in self.split_range_sized_subranges(current,
-                                                                  desired_size,
-                                                                  range_tracker):
-            yield split_key_range
-        start = sample_row.row_key
-      l = sample_row.offset_bytes
-
-  def range_split_fraction(self,
-                           current_size,
-                           desired_bundle_size,
-                           start_key,
-                           end_key):
-    range_tracker = LexicographicKeyRangeTracker(start_key, end_key)
-    return self.split_range_sized_subranges(current_size,
-                                            desired_bundle_size,
-                                            range_tracker)
-
-  def fraction_to_position(self, position, range_start, range_stop):
-    return LexicographicKeyRangeTracker.fraction_to_position(position,
-                                                             range_start,
-                                                             range_stop)
-
-  def split_range_sized_subranges(self,
-                                  sample_size_bytes,
-                                  desired_bundle_size,
-                                  ranges):
-
-    last_key = copy.deepcopy(ranges.stop_position())
-    s = ranges.start_position()
-    e = ranges.stop_position()
-
-    split_ = float(desired_bundle_size) / float(sample_size_bytes)
-    split_count = int(math.ceil(sample_size_bytes / desired_bundle_size))
-
-    for i in range(split_count):
-      estimate_position = ((i + 1) * split_)
-      position = self.fraction_to_position(estimate_position,
-                                           ranges.start_position(),
-                                           ranges.stop_position())
-      e = position
-      yield iobase.SourceBundle(sample_size_bytes * split_, self, s, e)
-      s = position
-    if not s == last_key:
-      yield iobase.SourceBundle(sample_size_bytes * split_, self, s, last_key )
-
-  def read(self, range_tracker):
-    if range_tracker.start_position() is not None:
-      if not range_tracker.try_claim(range_tracker.start_position()):
-        # there needs to be a way to cancel the request.
-        return
-    read_rows = self._getTable().read_rows(start_key=range_tracker.start_position(),
-      end_key=range_tracker.stop_position(),
-      filter_=self.beam_options['filter_'])
-
-    for row in read_rows:
-      self.read_row.inc()
-      yield row
-
-  def display_data(self):
-    ret = {
-      'projectId': DisplayDataItem(self.beam_options['project_id'],
-                                   label='Bigtable Project Id',
-                                   key='projectId'),
-      'instanceId': DisplayDataItem(self.beam_options['instance_id'],
-                                    label='Bigtable Instance Id',
-                                    key='instanceId'),
-      'tableId': DisplayDataItem(self.beam_options['table_id'],
-                                 label='Bigtable Table Id',
-                                 key='tableId')}
-    
-    return ret
-
-
-class ReadFromBigTable(beam.PTransform):
-  """ A transform to write to the Bigtable Table.
-
-  A PTransform that write a list of `DirectRow` into the Bigtable Table
-
-  """
-  def __init__(self, project_id, instance_id, table_id,
-               row_set=None,
-               filter_=None):
-    """ The PTransform to access the Bigtable Read Source
-    Args:
-      project_id(str): GCP Project of to write the Rows
-      instance_id(str): GCP Instance to write the Rows
-      table_id(str): GCP Table to write the `DirectRows`
-    """
-    self.beam_options = {'project_id': project_id,
-                         'instance_id': instance_id,
-                         'table_id': table_id,
-                         'row_set': row_set,
-                         'filter_': filter_}
-
-  def expand(self, pvalue):
-    beam_options = self.beam_options
-    return (pvalue
-            | 'ReadFromBigtable' >> beam.io.Read(_BigTableReadFn(beam_options['project_id'],
-                                                                 beam_options['instance_id'],
-                                                                 beam_options['table_id'],
-                                                                 row_set=beam_options['row_set'],
-                                                                 filter_=beam_options['filter_'])))
+__all__ = ['WriteToBigTable', 'ReadFromBigTable']
 
 
 class _BigTableWriteFn(beam.DoFn):
@@ -347,3 +147,183 @@ class WriteToBigTable(beam.PTransform):
             | beam.ParDo(_BigTableWriteFn(beam_options['project_id'],
                                           beam_options['instance_id'],
                                           beam_options['table_id'])))
+
+
+class _BigTableReadFn(iobase.BoundedSource):
+  """ Creates the connector to get the rows in Bigtable and using each
+  row in beam pipe line
+  Args:
+    project_id(str): GCP Project ID
+    instance_id(str): GCP Instance ID
+    table_id(str): GCP Table ID
+    row_set(RowSet): Creating a set of row keys and row ranges.
+    filter_(RowFilter): Filter to apply to cells in a row.
+
+  """
+  def __init__(self, project_id, instance_id, table_id,
+               row_set=None, filter_=None):
+    """ Constructor of the Read connector of Bigtable
+    Args:
+      project_id(str): GCP Project of to write the Rows
+      instance_id(str): GCP Instance to write the Rows
+      table_id(str): GCP Table to write the `DirectRows`
+    """
+    super(self.__class__, self).__init__()
+    self.beam_options = {'project_id': project_id,
+                         'instance_id': instance_id,
+                         'table_id': table_id,
+                         'row_set': row_set,
+                         'filter_': filter_}
+    self.table = None
+    self.read_row = Metrics.counter(self.__class__.__name__, 'read_row')
+
+  def _getTable(self):
+    if self.table is None:
+      client = Client(project=self.beam_options['project_id'])
+      instance = client.instance(self.beam_options['instance_id'])
+      self.table = instance.table(self.beam_options['table_id'])
+    return self.table
+
+  def get_sample_row_keys(self):
+    return self._getTable().sample_row_keys()
+
+  def get_range_tracker(self, start_position, stop_position):
+    return LexicographicKeyRangeTracker(start_position, stop_position)
+
+  def split(self,
+            desired_bundle_size,
+            start_position=None,
+            stop_position=None):
+
+    if self.beam_options['row_set'] is not None:
+      for sample_row_key in self.beam_options['row_set'].row_ranges:
+        sample_row_keys = self.get_sample_row_keys()
+        for row_split in self.split_range_size(desired_bundle_size,
+                                               sample_row_keys,
+                                               sample_row_key):
+          yield row_split
+    else:
+      suma = 0
+      last_offset = 0
+      current_size = 0
+
+      start_key = b''
+      end_key = b''
+
+      sample_row_keys = self.get_sample_row_keys()
+      for sample_row_key in sample_row_keys:
+        current_size = sample_row_key.offset_bytes-last_offset
+        if suma >= desired_bundle_size:
+          end_key = sample_row_key.row_key
+          for fraction in self.range_split_fraction(suma,
+                                                    desired_bundle_size,
+                                                    start_key, end_key):
+            yield fraction
+          start_key = sample_row_key.row_key
+
+          suma = 0
+        suma += current_size
+        last_offset = sample_row_key.offset_bytes
+
+  def split_range_size(self, desired_size, sample_row_keys, range_):
+    start, end = None, None
+    l = 0
+    for sample_row in sample_row_keys:
+      current = sample_row.offset_bytes - l
+      if sample_row.row_key == b'':
+        continue
+
+      if(range_.start_key <= sample_row.row_key and
+         range_.end_key >= sample_row.row_key):
+        if start is not None:
+          end = sample_row.row_key
+          range_tracker = LexicographicKeyRangeTracker(start, end)
+
+          for split_key_range in self.split_range_subranges(current,
+                                                            desired_size,
+                                                            range_tracker):
+            yield split_key_range
+        start = sample_row.row_key
+      l = sample_row.offset_bytes
+
+  def range_split_fraction(self,
+                           current_size,
+                           desired_bundle_size,
+                           start_key,
+                           end_key):
+    range_tracker = LexicographicKeyRangeTracker(start_key, end_key)
+    return self.split_range_subranges(current_size,
+                                      desired_bundle_size,
+                                      range_tracker)
+
+  def fraction_to_position(self, position, range_start, range_stop):
+    return LexicographicKeyRangeTracker.fraction_to_position(position,
+                                                             range_start,
+                                                             range_stop)
+
+  def split_range_subranges(self,
+                            sample_size_bytes,
+                            desired_bundle_size,
+                            ranges):
+
+    last_key = copy.deepcopy(ranges.stop_position())
+    s = ranges.start_position()
+    e = ranges.stop_position()
+
+    split_ = float(desired_bundle_size) // float(sample_size_bytes)
+    split_count = int(math.ceil(sample_size_bytes // desired_bundle_size))
+
+    for i in range(split_count):
+      estimate_position = ((i + 1) * split_)
+      position = self.fraction_to_position(estimate_position,
+                                           ranges.start_position(),
+                                           ranges.stop_position())
+      e = position
+      yield iobase.SourceBundle(sample_size_bytes * split_, self, s, e)
+      s = position
+    if not s == last_key:
+      yield iobase.SourceBundle(sample_size_bytes * split_, self, s, last_key)
+
+  def read(self, range_tracker):
+    if range_tracker.start_position() is not None:
+      if not range_tracker.try_claim(range_tracker.start_position()):
+        # there needs to be a way to cancel the request.
+        return
+    start_position = range_tracker.start_position()
+    stop_position = range_tracker.stop_position()
+    filter_ = self.beam_options['filter_']
+    read_rows = self._getTable().read_rows(start_key=start_position,
+                                           end_key=stop_position,
+                                           filter_=filter_)
+
+    for row in read_rows:
+      self.read_row.inc()
+      yield row
+
+  def display_data(self):
+    ret = {'projectId': DisplayDataItem(self.beam_options['project_id'],
+                                        label='Bigtable Project Id',
+                                        key='projectId'),
+           'instanceId': DisplayDataItem(self.beam_options['instance_id'],
+                                         label='Bigtable Instance Id',
+                                         key='instanceId'),
+           'tableId': DisplayDataItem(self.beam_options['table_id'],
+                                      label='Bigtable Table Id',
+                                      key='tableId')}
+    return ret
+
+
+class ReadFromBigTable(beam.PTransform):
+  def __init__(self, project_id, instance_id, table_id):
+    self.beam_options = {'project_id': project_id,
+                         'instance_id': instance_id,
+                         'table_id': table_id}
+
+  def expand(self, pbegin):
+    project_id = self.beam_options['project_id']
+    instance_id = self.beam_options['instance_id']
+    table_id = self.beam_options['table_id']
+    return (pbegin
+            | 'ReadFromBigtable' >> beam.io.Read(_BigTableReadFn(project_id,
+                                                                 instance_id,
+                                                                 table_id)))
---
+++ test_million_rows_both.py
@@ -0,0 +1,99 @@
+from __future__ import absolute_import
+import argparse
+import datetime
+import uuid
+
+
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import SetupOptions
+from apache_beam.testing.util import assert_that
+from apache_beam.testing.util import equal_to
+
+from google.cloud._helpers import _microseconds_from_datetime
+from google.cloud._helpers import UTC
+
+from beam_bigtable import ReadFromBigTable
+
+
+EXISTING_INSTANCES = []
+LABEL_KEY = u'python-bigtable-beam'
+label_stamp = datetime.datetime.utcnow().replace(tzinfo=UTC)
+label_stamp_micros = _microseconds_from_datetime(label_stamp)
+LABELS = {LABEL_KEY: str(label_stamp_micros)}
+
+
+class PrintKeys(beam.DoFn):
+  def __init__(self):
+    from apache_beam.metrics import Metrics
+    self.print_row = Metrics.counter(self.__class__.__name__, 'print_row')
+
+  def __setstate__(self, options):
+    from apache_beam.metrics import Metrics
+    self.print_row = Metrics.counter(self.__class__.__name__, 'print_row')
+
+  def process(self, row):
+    self.print_row.inc()
+    return [row]
+
+
+def run(argv=[]):
+  project_id = 'grass-clump-479'
+  instance_id = 'python-write'
+  DEFAULT_TABLE_PREFIX = "python-test"
+  #table_id = DEFAULT_TABLE_PREFIX + "-" + str(uuid.uuid4())[:8]
+  guid = str(uuid.uuid4())[:8]
+  table_id = 'testmillion6bd104b8'
+  jobname = 'read-' + table_id + '-' + guid
+  
+
+  argv.extend([
+    '--experiments=beam_fn_api',
+    '--project={}'.format(project_id),
+    '--instance={}'.format(instance_id),
+    '--table={}'.format(table_id),
+    '--projectId={}'.format(project_id),
+    '--instanceId={}'.format(instance_id),
+    '--tableId={}'.format(table_id),
+    '--job_name={}'.format(jobname),
+    '--requirements_file=requirements.txt',
+    '--runner=dataflow',
+    '--autoscaling_algorithm=NONE',
+    '--num_workers=10',
+    '--staging_location=gs://juantest/stage',
+    '--temp_location=gs://juantest/temp',
+    '--setup_file=/usr/src/app/example_bigtable_beam/beam_bigtable_package/setup.py',
+    '--extra_package=/usr/src/app/example_bigtable_beam/beam_bigtable_package/dist/beam_bigtable-0.3.24.tar.gz'
+  ])
+  parser = argparse.ArgumentParser(argv)
+  parser.add_argument('--projectId')
+  parser.add_argument('--instanceId')
+  parser.add_argument('--tableId')
+  (known_args, pipeline_args) = parser.parse_known_args(argv)
+
+  print('ProjectID:',project_id)
+  print('InstanceID:',instance_id)
+  print('TableID:',table_id)
+  print('JobID:', jobname)
+
+  pipeline_options = PipelineOptions(argv)
+  pipeline_options.view_as(SetupOptions).save_main_session = True
+
+  
+  config_data = {'project_id': project_id,
+                 'instance_id': instance_id,
+                 'table_id': table_id}
+  with beam.Pipeline(options=pipeline_options) as p:
+    row_count = 10000000
+    row_step = row_count if row_count <= 10000 else row_count/10000
+    (p
+     | 'Ranges' >> beam.Create([(str(i),str(i+row_step)) for i in xrange(0, row_count, row_step)])
+     | 'BigtableFromRead' >> ReadFromBigTable(project_id=config_data['project_id'],
+                                              instance_id=config_data['instance_id'],
+                                              table_id=config_data['table_id']))
+    result = p.run()
+    result.wait_until_finish()
+
+
+if __name__ == '__main__':
+  run()
